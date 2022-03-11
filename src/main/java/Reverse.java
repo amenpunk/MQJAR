3aaccfe43ea7f95a8b27107dd59ac0eb968bd8d7
amenpunk <ondasycircuitos@gmail.com>
2022-03-10 20:38:06 -0600

    test

---
+++ src/main/java/Reverse.java
@@ -0,0 +1,18 @@
+// simple future -> study
+// what are they going to study at university 
+// they are going to study medicine at university
+
+public class Reverse{
+    static void Run(){
+
+        String temp = "";
+
+        for(int i = a.length(); i > 0; i = i -1){
+            char letter = a.charAt(i- 1);
+            temp = temp + letter;
+            System.out.println(temp);
+        }
+
+    }
+}
+
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,6 +1,9 @@
+
 public class Start{
+
     public static void main (String[] args ){
-        String message = "Hola mundo";
-        System.out.println(message);
+        System.out.println("Start Class");
+        Reverse.Run();
     }
+
 }
